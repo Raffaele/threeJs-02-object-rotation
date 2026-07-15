834295d18626d11980f3d80e7fa1635a24c2dcf3
Raffaele <mori.raffaele@gmail.com>
2026-07-15 23:07:27 +0100

    Stars background added.

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+const universeTextureLoader = new THREE.TextureLoader();
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const starsUrl = new URL('./imgs/stars.jpg', import.meta.url).toString();
+
+const scene = new THREE.Scene();
+universeTextureLoader.load(
+  starsUrl,
+  (texture) => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+    scene.background = texture;
+  },
+  undefined, (error) => {
+    console.error('Error loading texture:', error);
+  });
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const sunGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+scene.add(sun);
+
+
+function continuuouslyRender(callback: () => void) {
+  callback();
+  requestAnimationFrame(() => continuuouslyRender(callback));
+}
+
+continuuouslyRender(() => renderer.render(scene, camera));
+
