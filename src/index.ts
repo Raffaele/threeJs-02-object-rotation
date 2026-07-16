1916a6f47fc1009dc75fb902c3e8e2605adba205
Raffaele <mori.raffaele@gmail.com>
2026-07-16 23:02:37 +0100

    Changed TextureLoader into CubeTextureLoader.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const universeTextureLoader = new THREE.TextureLoader();
+const universeTextureLoader = new THREE.CubeTextureLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const starsUrl = new URL('./imgs/stars.jpg', import.meta.url).toString();
+const stars1Url = new URL('./imgs/stars.jpg', import.meta.url).toString();
 
 const scene = new THREE.Scene();
 universeTextureLoader.load(
-  starsUrl,
+  [stars1Url, stars1Url, stars1Url, stars1Url, stars1Url, stars1Url],
   (texture) => {
     texture.colorSpace = THREE.SRGBColorSpace;
     scene.background = texture;
@@ -20,6 +21,9 @@ universeTextureLoader.load(
   });
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
 
 const sunGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
